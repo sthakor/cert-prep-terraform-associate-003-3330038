3eccf76cde98d6ef90b682d8cd1b36892d7a8836
Sanjiv Thakor <sanjiv@example.com>
2025-01-16 20:22:25 -0600

    modified main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
  resource "aws_instance" "ec2-instance1" {
-   ami = "ami-0005e0cfe09cc9050"
+   ami = "ami-0df8c184d5f6ae949"
    instance_type = "t2.micro"
 
    tags = {                                
      Name = "demo-server1"
-}
+  }
 }
 
 
 resource "aws_instance" "ec2-Instance2" {
   provider      = aws.west
-  ami           = "ami-0a5ed7a812aeb495a"  # AMI for us-west-1
+  ami           = "ami-0118dc74bd8f98b13"  # AMI for us-west-1
   instance_type = "t2.micro"
 
   tags = {                                
     Name = "demo-server2"
-}
+  }
 }
