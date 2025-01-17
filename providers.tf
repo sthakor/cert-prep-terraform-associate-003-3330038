1fe4ed3d39f65eb7998a8d2bb80c86f39ee888eb
Sanjiv Thakor <sanjiv@example.com>
2025-01-17 07:32:37 -0600

    Updated profile name in providers.tf

--- providers.tf
+++ providers.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  profile = "Helen"
+  profile = "sthakor"
   # Configuration options
 }
 
 provider "aws" {
   alias = "west"
   region = "us-west-1"
-  profile = "Helen"
+  profile = "sthakor"
 }
 
